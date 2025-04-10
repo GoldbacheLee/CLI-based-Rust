1451c9186288035cc0f6163cb74e274e2dc95c21
lee <goldbache@yandex.com>
2025-04-10 14:17:13 +0800

    keep coding

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,14 @@
-use std::fs;
-use csv::Reader;
 use clap::Parser;
-use rcli::{Opts, Subcommand};
-use serde::{Serialize,Deserialize};
+use rcli::{process_csv, Opts, Subcommand};
 
 // rcli  csv  -i   input.csv   -o  output.json   --header  -d "."
 
-#[derive(Debug,Deserialize,Serialize)]
-#[serde(rename_all = "PascalCase")]
-struct Player{
-    #[serde(rename = "Name")]
-    name:String,
-    #[serde(rename = "Position")]
-    position: String,
-    #[serde(rename = "DOB")]
-    dob: String,
-    #[serde(rename = "Nationality")]
-    nationality: String,
-    #[serde(rename = "Kit Number")]
-    kit_number: String
-}
-
-
 fn main() -> anyhow::Result<()>{
     let opts = Opts ::parse();
-    println!("{:?}",    opts);
+    //println!("{:?}",    opts);
     match  opts.cmd{
-        Subcommand::Csv(opts) => {
-            let mut    reader = Reader::from_path(opts.input)?;
-            // let records = reader
-            //     .deserialize()
-            //     .map(|record| record.unwrap())
-            //     .collect::<Vec<Player>>();
-
-            let mut ret = Vec::with_capacity(128);
-
-
-            for result in reader.deserialize() {
-                 let record : Player = result?;
-                ret.push(record);
-            }
-            
-            let json = serde_json::to_string_pretty(&ret)?;
-            fs::write(opts.output, json)?;
-        }
+        Subcommand::Csv(opts) =>  process_csv(&opts.input,&opts.output)?,
+       
     }
     Ok(())
 }
-
---
+++ src/process.rs
@@ -0,0 +1,42 @@
+use std::fs;
+use csv::Reader;
+use serde::{Serialize,Deserialize};
+use anyhow::Result;
+
+
+
+#[derive(Debug,Deserialize,Serialize)]
+#[serde(rename_all = "PascalCase")]
+struct Player{
+    #[serde(rename = "Name")]
+    name:String,
+    #[serde(rename = "Position")]
+    position: String,
+    #[serde(rename = "DOB")]
+    dob: String,
+    #[serde(rename = "Nationality")]
+    nationality: String,
+    #[serde(rename = "Kit Number")]
+    kit_number: String
+}
+
+
+pub fn process_csv(input:&str,output:&str) -> Result<()> {
+    let mut    reader = Reader::from_path(input)?;
+    // let records = reader
+    //     .deserialize()
+    //     .map(|record| record.unwrap())
+    //     .collect::<Vec<Player>>();
+
+    let mut ret = Vec::with_capacity(128);
+
+
+    for result in reader.deserialize() {
+         let record : Player = result?;
+        ret.push(record);
+    }
+    
+    let json = serde_json::to_string_pretty(&ret)?;
+    fs::write(output, json)?;
+    Ok(())
+}
